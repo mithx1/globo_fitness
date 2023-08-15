4967760835d1af0e61e36f8a05805bb7d106f553
Julius <71910804+mithx1@users.noreply.github.com>
2023-08-16 05:04:32 +0800

    left navigation

---
+++ lib/shared/menu_drawer.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/add_item.dart';
+import 'package:globo_fitness/screens/bmi_screen.dart';
+import 'package:globo_fitness/screens/intro_screen.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: buildMenuItems(context),
+      ),
+    );
+  }
+
+  List<Widget> buildMenuItems(BuildContext context) {
+    final List<String> menuTitles = [
+      'Home',
+      'Inventory',
+      'Menu',
+      'Add an Item'
+    ];
+
+    List<Widget> menuItems = [];
+    menuItems.add(const DrawerHeader(
+      decoration: BoxDecoration(color: Colors.blueGrey),
+      child: Text(
+        'Ordering App',
+        style: TextStyle(color: Colors.white, fontSize: 28),
+      ),
+    ));
+
+    menuTitles.forEach((String element) {
+      Widget screen = Container();
+      menuItems.add(ListTile(
+        title: Text(element, style: TextStyle(fontSize: 18)),
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = const IntroScreen();
+              break;
+            case 'Inventory':
+              screen = const BmiScreen();
+              break;
+            case 'Add an Item':
+              screen = const AddItem();
+              break;
+          }
+          Navigator.of(context).pop();
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => screen));
+        },
+      ));
+    });
+
+    return menuItems;
+  }
+}
