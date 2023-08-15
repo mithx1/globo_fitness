3bbd930e5e4711ecb79b5dc843d4dc3e078e62ea
Julius <71910804+mithx1@users.noreply.github.com>
2023-08-16 05:03:13 +0800

    pushNamed implementation bottom navigation implementation sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/bmi_screen.dart';
 import 'package:globo_fitness/screens/intro_screen.dart';
 
 void main() {
@@ -6,13 +7,17 @@ void main() {
 }
 
 class GlobeApp extends StatelessWidget {
-  const GlobeApp({super.key});
+  const GlobeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: IntroScreen(),
+      routes: {
+        '/': (context) => IntroScreen(),
+        '/bmi': (context) => BmiScreen()
+      },
+      initialRoute: '/',
     );
   }
 }
